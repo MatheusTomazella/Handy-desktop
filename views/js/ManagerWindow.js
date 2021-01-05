e9b91c7b0ac3f8d4c5ec2676491ace0dd213d60a
MatheusTomazella <matheustomazella250504@gmail.com>
2021-01-05 02:48:30 -0300

    2021-01-04

--- views/js/ManagerWindow.js
+++ views/js/ManagerWindow.js
@@ -4,21 +4,33 @@ class ManagerWindow {
         this.route = args.route;
         document.getElementById( 'query-title' ).innerText = `Gerenciar ${args.title}`;
         
-        this.queryList = document.getElementById( 'query' );
+        this.queryList      = document.getElementById( 'query' );
+        this.typeSelect     = $('#search-type');
+        this.orderSelect    = $('#search-order');
 
         const waitingToken = setInterval( async ( ) => {
             if ( admin.token ) { 
                 clearInterval( waitingToken );
-                await this[ `construct_${this.type}` ]( );
-                this.requestQuery( this.type, this.route, admin.token );
+                await this.constructDefault( );
+                this.requestQuery( this.type, this.route, args.search );
             }
         }, 500 );
         return this;
     }
-    requestQuery ( type, route, token ) {
+
+    constructDefault ( ) {
+        this[ `construct_${this.type}` ]( );
+    }
+
+    requestQuery ( type, route, query ) {
+        let queryString = `token=${admin.token}`;
+        if ( query ) {
+            if ( query.key )    queryString += `&${query.key}=${query.value}`;
+            if ( query.order )  queryString += `&order=${query.order}`;
+        }
         $.ajax({
             type: "GET",
-            url: `${APIurl}/${route}?token=${token}`,
+            url: `${APIurl}/${route}?${queryString}`,
             success: ( response ) => {
                 this[ `fill_${type}` ]( response );
             }, 
@@ -30,53 +42,66 @@ class ManagerWindow {
     }
 
     construct_user ( ) {
-        this.queryList.innerHTML = 
-        `<li class="query-list row">
-            <div class="query-list-title col s1"> Id     </div>
-            <div class="query-list-title col s4"> Nome   </div>
-            <div class="query-list-title col s4"> Email  </div>
-            <div class="query-list-title col s3"> Senha  </div>
-        </li>`
+        this.construct( { Id: 1, Nome: 4, Email: 4, Senha: 3 },
+            { 
+                type:   { Id: 'ID', name: 'Nome', email: "Email", password: "Senha" },
+                order:  { none: 'Não Ordenar', id: 'ID', name: "Nome", email: 'Email', password: 'Senha' }
+            } );
     }
     construct_admin ( ) {
-        this.queryList.innerHTML = 
-        `<li class="query-list row">
-            <div class="query-list-title col s1"> Id     </div>
-            <div class="query-list-title col s3"> Nome   </div>
-            <div class="query-list-title col s4"> Email  </div>
-            <div class="query-list-title col s3"> Senha  </div>
-            <div class="query-list-title col s1"> Nível  </div>
-        </li>`
+        this.construct( { Id: 1, Nome: 3, Email: 4, Senha: 3, Nível: 1 },
+            { 
+                type:   { Id: 'ID', name: 'Nome', email: "Email", password: "Senha", accessLevel: "Nível de acesso" },
+                order:  { none: 'Não Ordenar', id: 'ID', name: "Nome", email: 'Email', password: 'Senha', accessLevel: "Nível de acesso" }
+            } );
     }
     construct_component ( ) {
-        this.queryList.innerHTML = 
-        `<li class="query-list row">
-            <div class="query-list-title col s1"> Id        </div>
-            <div class="query-list-title col s5"> Nome      </div>
-            <div class="query-list-title col s6"> Descrição </div>
-        </li>`
+        this.construct( { Id: 1, Nome: 5, Descrição: 6 },
+            { 
+                type:   { Id: 'ID', name: 'Nome', descr: "Descrição" },
+                order:  { none: 'Não Ordenar', id: 'ID', name: "Nome", descr: 'Descrição' }
+            } );
     }
     construct_provider ( ) {
-        this.queryList.innerHTML = 
-        `<li class="query-list row">
-            <div class="query-list-title col s1"> Id        </div>
-            <div class="query-list-title col s4"> Nome      </div>
-            <div class="query-list-title col s1"> IdComp    </div>
-            <div class="query-list-title col s5"> Link      </div>
-            <div class="query-list-title col s1"> Custo     </div>
-        </li>`
+        this.construct( { Id: 1, Nome: 4, IdComp: 1, Link: 5, Custo: 1 },
+            { 
+                type:   { Id: 'ID', name: 'Nome', idComp: "ID do Componente", cost: "Custo" },
+                order:  { none: 'Não Ordenar', id: 'ID', name: "Nome", idComp: "ID do Componente", cost: "Custo" }
+            } );
     }
     construct_message ( ) {
-        this.queryList.innerHTML = 
-        `<li class="query-list row">
-            <div class="query-list-title col s1"> Id        </div>
-            <div class="query-list-title col s1"> IdUser    </div>
-            <div class="query-list-title col s1"> IdAdmin   </div>
-            <div class="query-list-title col s1"> Emissor   </div>
-            <div class="query-list-title col s5"> Texto     </div>
-            <div class="query-list-title col s3"> Data      </div>
-        </li>`
+        this.construct( { Id: 1, IdUser: 1, IdAdmin: 1, Emissor: 1, Texto: 5, Data: 3 },
+            { 
+                type:   { Id: 'ID', idUser: 'ID do Usuário', idAdmin: "ID do Suporte", sender: "Emissor", text: 'Texto', datetime: "Data", type: "Tipo da Mensagem", image: "Imagem" },
+                order:  { none: 'Não Ordenar', Id: 'ID', idUser: 'ID do Usuário', idAdmin: "ID do Suporte", sender: "Emissor", text: 'Texto', datetime: "Data", type: "Tipo da Mensagem", image: "Imagem"  }
+            } );
     }
+
+    async construct ( blueprint, selectBlueprints = { type, order } ) {
+        this.queryList.innerHTML = '';
+        const li = this.generateLi( undefined, true );
+        const fields = Object.keys( blueprint );
+        await fields.forEach( async field => {
+            li.appendChild( await this.generateDiv( blueprint[ field ], field, true ) );
+        } );
+        this.queryList.appendChild( li );
+        if ( selectBlueprints ) { 
+            this.populateSelect( this.typeSelect, selectBlueprints.type );
+            this.populateSelect( this.orderSelect, selectBlueprints.order );
+        }
+    }
+    populateSelect ( select, blueprint ) {
+        select.empty();
+        const values = Object.keys( blueprint );
+        values.forEach( value => {
+            const option = document.createElement( 'option' );
+            option.value = value;
+            option.innerText = blueprint[ value ];
+            select.append( option );
+        } )
+        $('select').formSelect();
+    }
+
 
     fill_user ( query ) {
         this.fill ( query, { id: 1, name: 4, email: 4, password: 3 } );
@@ -99,45 +124,29 @@ class ManagerWindow {
         query.forEach( async item => {
             const li = this.generateLi( );
             await fields.forEach( async field => {
-                console.log( field, blueprint, item, blueprint[field], item[field] );
                 li.appendChild( await this.generateDiv( blueprint[ field ], item[ field ] ) );
             } );
             this.queryList.appendChild( li );
         } );
     }
 
-    generateLi ( queryData ) {
+    generateLi ( queryData, isTitle = false ) {
         const element = document.createElement( 'li' );
-        element.className = 'users-waiting query-item row';
+        const className = ( isTitle ) ? 'query-list' : 'query-item';
+        element.className = `users-waiting ${className} row`;
         //element.dataset.queryData = data;
-        element.onmousedown = ( event ) => {
+        if ( !isTitle ) element.onmousedown = ( event ) => {
             const position  = { top: event.clientY, left: event.clientX };
             const user      = {  };
             contextMenu.show( position, user );
         };
         return element;
     }
-    generateDiv ( colSize, text ) {
+    generateDiv ( colSize, text, isTitle = false ) {
         const div = document.createElement( 'div' );
-        div.className = `query-item col s${colSize}`;
+        const className = ( isTitle ) ? 'query-list-title' : 'query-item';
+        div.className = `${className} col s${colSize}`;
         div.innerText = text;
         return div;
-    }
-
-    showQuery ( query ) {
-        // query.forEach( element => {
-        //     const li = document.createElement( 'li' );
-        //     li.className = 'queue-user';
-        //     li.innerText = `${element.userId}: ${element.name}`;
-        //     li.dataset.id     = element.userId;
-        //     li.dataset.socket = element.socketId;
-        //     li.onmousedown = ( event ) => {
-        //         const position  = { top: event.clientY, left: event.clientX };
-        //         const user      = { id: event.target.dataset.id, socket: event.target.dataset.socket, name: element.name };
-        //         conte.show( position, user );
-        //     }
-        //     supportQueueList.appendChild( li );
-        //     this.queryList;
-        // } );
     }
 }
